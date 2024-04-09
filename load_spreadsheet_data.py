e9962c58ec0a49e8ee1d2c677c3f51772b9780ce
hajiy25n <166480597+hajiy25n@users.noreply.github.com>
2024-04-09 16:34:23 +0900

    Add files via upload

--- load_spreadsheet_data.py
+++ load_spreadsheet_data.py
@@ -10,15 +10,10 @@ def load_spreadsheet_data(json_file_path, spreadsheet_url, sheetname):
     cell_data = worksheet.get('A1:C100')
 
     # Construct Data Frame
-    import pandas as pd
-
-    # 셀 데이터
-    # 모든 행의 길이를 헤더의 길이와 동일하게 맞춤
     max_columns = len(cell_data[0])
-    formatted_data = [row + [None] * (max_columns - len(row)) for row in cell_data]
+    formatted_data = [row + [None] * (max_columns - len(row)) for row in cell_data]     # Customized the length of all rows of all rows
     patient_data = pd.DataFrame(formatted_data[1:], columns=formatted_data[0])
 
-    #patient_data = pd.DataFrame(cell_data[1:], columns=cell_data[0])
     first_empty_row = patient_data[patient_data['Group'].isnull()].iloc[0]['ID']
     # Column of Factor
     last_col = 'B' + str(int(first_empty_row) + 1)
