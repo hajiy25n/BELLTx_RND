b420beeba2de17c1723b5190354ed0f6af7cd72b
hajiy25n <166480597+hajiy25n@users.noreply.github.com>
2024-04-11 13:46:49 +0900

    Update 4f_Stratified RND_BELL.py

--- 4f_Stratified RND_BELL.py
+++ 4f_Stratified RND_BELL.py
@@ -1,4 +1,4 @@
-
+# BELL Tx's RND Source
 
 from updateTosheet import *
 from load_spreadsheet_data import *
@@ -12,7 +12,7 @@ max_group_size = 15     # Maximum number of assignments that can be allocated to
 group_len = 3       # Total number of groups available for assignments
 
 # Main execution
-num, factor, patient_data = load_spreadsheet_data(json_file_path, spreadsheet_url,sheetname)
+num, factor, patient_data = load_spreadsheet_data(json_file_path, spreadsheet_url,sheetname)  # Need to check the format of spreadsheet
 print(num + '-'  + factor)     # Check the current factor
 
 assigned_group = dynamic_stratified_randomization(patient_data, factor, max_group_size, group_len)
